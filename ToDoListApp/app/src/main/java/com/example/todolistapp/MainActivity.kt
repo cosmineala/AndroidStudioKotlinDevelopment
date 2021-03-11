d87249a3cb3ed188543627357f0b937003a02459
CapisizuCosmin <cosmineala@gmail.com>
2021-03-11 19:01:25 +0200

    ToDoListApp => Implement list V 1.0
    1 Data model
    2 Adapter
    3 ViewHolder
    4 Main Activity implementation

--- ToDoListApp/app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ ToDoListApp/app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.todolistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter_ToDo: Adapter_ToDo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter_ToDo = Adapter_ToDo( mutableListOf() )
+        rvToDoItems.adapter = adapter_ToDo
+        rvToDoItems.layoutManager = LinearLayoutManager( this )
+
+        btAddToToDo.setOnClickListener {
+            val toDoTitle = etToDoInput.text.toString()
+            if ( toDoTitle.isNotEmpty() ){
+                val toDo = Model_ToDo( toDoTitle )
+                adapter_ToDo.AddToDo( toDo )
+                etToDoInput.text.clear()
+            }else{
+                Toast.makeText(this,"No data entered",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btDeleteSelected.setOnClickListener {
+            adapter_ToDo.DelToDo()
+        }
+
+
+
     }
 }
---
+++ ToDoListApp/app/src/main/java/com/example/todolistapp/Model_ToDo.kt
@@ -0,0 +1,6 @@
+package com.example.todolistapp
+
+data class Model_ToDo (
+        val title: String,
+        var isChecked: Boolean = false
+)
